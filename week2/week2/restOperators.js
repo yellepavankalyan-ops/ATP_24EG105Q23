aa3c3ca9973d69d50d7510bc9863aa608e5c594d
yellepavankalyan-ops <yellepavankalyan7@gmail.com>
2026-03-10 12:16:30 +0530

    Delete week2/week2 directory

--- week2/week2/restOperators.js
+++
@@ -1,5 +0,0 @@
-// 💡 Exercise 3: Create a function that receives any number of args as arguments and return their sum using REST parameter
-function findSum(...a){
-    console.log("sum of the elements",a.reduce((sum,ele)=>sum+ele))
-}
-findSum(10,20,30)
