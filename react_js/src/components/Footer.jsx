0a31b5975dc8f75920377ce25d152f024e931e64
yellepavankalyan-ops <yellepavankalyan7@gmail.com>
2026-03-24 21:12:48 +0530

    Add files via upload

---
+++ react_js/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer(){
+    return (
+        <div className="bg-gray-300 p-10 flex justify-around">
+            <div>
+                <p>Hyderabad</p>
+                <p>Telangana</p>
+            </div>
+            <div>
+                <p>India</p>
+                <p>500001</p>
+            </div>
+            <div>
+                <p>Contact</p>
+                <p>Email</p>
+            </div>
+        </div>
+    )
+} 
+
+export default Footer;
