a26a9cb7760f31986898e5b40f106637e0f5684d
yellepavankalyan-ops <yellepavankalyan7@gmail.com>
2026-05-14 15:12:07 +0530

    Fix production frontend API URLs

--- week7/BLOGAPP/frontend/src/components/AuthorArticles.jsx
+++ week7/BLOGAPP/frontend/src/components/AuthorArticles.jsx
@@ -33,7 +33,7 @@ function AuthorArticles() {
       try {
         setLoading(true);
         //read articles of current author
-        let res = await axios.get(" http://localhost:5000/author-api/articles", { withCredentials: true });
+        let res = await axios.get("http://localhost:5000/author-api/articles", { withCredentials: true });
         if (res.status === 200) {
           setArticles(res.data.payload);
         }
