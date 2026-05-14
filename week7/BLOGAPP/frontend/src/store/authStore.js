1e77141bf5350314d57fc5d5c545d159a17e4a66
yellepavankalyan-ops <yellepavankalyan7@gmail.com>
2026-05-14 15:00:43 +0530

    Fix frontend backend URL

--- week7/BLOGAPP/frontend/src/store/authStore.js
+++ week7/BLOGAPP/frontend/src/store/authStore.js
@@ -60,7 +60,7 @@ export const useAuth = create((set) => ({
   checkAuth: async () => {
     try {
       set({ loading: true });
-      const res = await axios.get("http://localhost:5000/auth/check-auth", { withCredentials: true });
+      const res = await axios.get("https://blog-backend-i6s7.onrender.com/auth/check-auth", { withCredentials: true });
 
       set({
         currentUser: res.data.payload,
